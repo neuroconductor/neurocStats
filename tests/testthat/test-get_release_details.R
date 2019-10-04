1e0fb5ca2f13d9f25021844fba851191993744d2
adigherman <adig@jhu.edu>
2019-10-04 11:03:57 -0400

    fixing tests and submit_package_release

--- tests/testthat/test-get_release_details.R
+++ tests/testthat/test-get_release_details.R
@@ -1,8 +1,8 @@
 test_that("get release details", {
 
   testthat::expect_message(get_release_details("releases/2017/oct"),"Invalid Neuroconductor Release")
-  testthat::expect_is(get_release_details("releases/2017/nov"),"data.frame")
-  testthat::expect_is(get_release_details(year = "2017", month = "nov"),"data.frame")
-  testthat::expect_is(get_release_details(year = "2017", month = "nov", os = "osx"),"data.frame")
-  testthat::expect_equal(nrow(get_release_details(year = "2017", month = "nov", os = "osx")), 55)
+  testthat::expect_is(get_release_details("releases/2017/11"),"data.frame")
+  testthat::expect_is(get_release_details(year = "2017", month = "11"),"data.frame")
+  testthat::expect_is(get_release_details(year = "2017", month = "11", os = "osx"),"data.frame")
+  testthat::expect_equal(nrow(get_release_details(year = "2017", month = "11", os = "osx")), 55)
 })
